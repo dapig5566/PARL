9c057ddd66409a1fd097a3c2a359f0f19cd4f88a
dapig5566 <dapig5566@gmail.com>
2023-11-09 01:32:21 +0800

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,7 +1,7 @@
 WEIGHT_PATH=weights/pretrain/TDQN_new_datadist_v4
 CKPT_PATH=models/1699238998_PARL_ptv4_rand_newreprod/checkpoint_001360
 CONFIG_PATH=config/parl.json
-MODEL=PARL # the same class as it is in ckpt_path. possible models: A2C, SAC, AlphaZero, DQN
+MODEL=PARL # the same class as it is in ckpt_path. possible models: PARL, A2C, SAC, AlphaZero, DQN
 
 python run.py \
     --do_eval \
