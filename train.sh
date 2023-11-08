c499d3d42b62095b0f72b749d477a76ac8f05e75
dapig5566 <dapig5566@gmail.com>
2023-11-09 01:32:05 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 NUM_STEPS=2000
 WEIGHT_PATH=weights/pretrain/TDQN_new_datadist_v4
-CONFIG_PATH=config/dqn.json
-MODEL=DQN # possible models: A2C, SAC, AlphaZero, DQN
+CONFIG_PATH=config/parl.json
+MODEL=PARL # possible models: PARL, A2C, SAC, AlphaZero, DQN
 EXP_NAME=ptv4_rand_reprod
 
 python run.py \
